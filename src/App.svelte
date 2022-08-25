768a1af28f080102efe7a4d1cce233d532f97de5
dlamon1 <devon.lamond@gmail.com>
2022-08-25 15:46:02 -0700

    init commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { canvasWrapperHeight, canvasWrapperWidth, isChrome } from "./store";
   import HandleColorLabelUpdates from "./Handle.ColorLabelUpdates.svelte";
   import Toolbar from "./Toolbar.svelte";
